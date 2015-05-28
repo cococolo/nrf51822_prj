1ca7a7a482f5ff511cd7f49144f45d144c238eed
cococolo <zhujiang373100513@163.com>
2015-05-28 11:56:31 +0800

    new prj

---
+++ mdk/RTE/RTE_Components.h
@@ -0,0 +1,15 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'TEST' 
+ * Target:  'NordicTest' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+#define BSP_DEFINES_ONLY
+
+#endif /* RTE_COMPONENTS_H */
---
+++ mdk/main.d
@@ -0,0 +1,16 @@
+.\main.o: ..\src\main.c
+.\main.o: C:\Keil_v5\ARM\ARMCC\bin\..\include\stdbool.h
+.\main.o: C:\Keil_v5\ARM\ARMCC\bin\..\include\stdint.h
+.\main.o: C:\Keil_v5\ARM\PACK\NordicSemiconductor\nRF_Drivers\3.0.0\hal\nrf_delay.h
+.\main.o: C:\Keil_v5\ARM\PACK\NordicSemiconductor\nRF_DeviceFamilyPack\7.2.1\Device\Include\nrf.h
+.\main.o: C:\Keil_v5\ARM\PACK\NordicSemiconductor\nRF_DeviceFamilyPack\7.2.1\Device\Include\nrf51.h
+.\main.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\4.1.0\CMSIS\Include\core_cm0.h
+.\main.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\4.1.0\CMSIS\Include\core_cmInstr.h
+.\main.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\4.1.0\CMSIS\Include\core_cmFunc.h
+.\main.o: C:\Keil_v5\ARM\PACK\NordicSemiconductor\nRF_DeviceFamilyPack\7.2.1\Device\Include\system_nrf51.h
+.\main.o: C:\Keil_v5\ARM\PACK\NordicSemiconductor\nRF_DeviceFamilyPack\7.2.1\Device\Include\nrf51_bitfields.h
+.\main.o: C:\Keil_v5\ARM\PACK\NordicSemiconductor\nRF_DeviceFamilyPack\7.2.1\Device\Include\nrf51_deprecated.h
+.\main.o: C:\Keil_v5\ARM\PACK\NordicSemiconductor\nRF_DeviceFamilyPack\7.2.1\Device\Include\compiler_abstraction.h
+.\main.o: C:\Keil_v5\ARM\PACK\NordicSemiconductor\nRF_Drivers\3.0.0\hal\nrf_gpio.h
+.\main.o: C:\Keil_v5\ARM\PACK\NordicSemiconductor\nRF_Examples\8.1.0\bsp\boards.h
+.\main.o: C:\Keil_v5\ARM\PACK\NordicSemiconductor\nRF_Examples\8.1.0\bsp\pca10028.h
